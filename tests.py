5650943efa3d3b4db1675cc299e50340e81499ab
Eric4Jiang <ericjiang378@gmail.com>
2019-06-05 21:29:07 -0400

    Fixed tests.py to not delete database.

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ class TestConfig(Config):
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app()
+        self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
@@ -36,10 +36,20 @@ class UserModelCase(unittest.TestCase):
         self.assertTrue(user.verify_password(password))
         self.assertFalse(user.verify_password(wrong_password))
 
-    def test_user_and_datasets(self):
-        pass
 
-    def test_datasets_and_tags(self):
+class DatasetModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_dataset_text_search(self):
         pass
 
 
